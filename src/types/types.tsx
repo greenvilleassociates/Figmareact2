7165f8effc160937d4723b7e296b8299ce2a3955
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-03-07 12:45:28 -0500

    V10.01

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -74,3 +74,31 @@ export interface LoginFormValues {
 export interface RegisterFormValues extends LoginFormValues {
   name: string
 }
+// Allow importing image assets
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
